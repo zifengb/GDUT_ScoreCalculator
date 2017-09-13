cb60191ce65963eb59c48f08877a4352abe4e207
zifengb <987400803@qq.com>
2017-09-13 10:22:37 +0800

    #优化绩点计算与展示

--- calcScore.js
+++ calcScore.js
@@ -2,64 +2,9 @@
  * [计算绩点方法]
  * @author zifengb
  * @createTime 2017-07-27T22:13:36+0800
- * @param      {[array object]}  scoreJson
- * @return     {[number]}
+ * @param      {string}  scoreString
+ * @return     {[object]}
  */
-
-/**
- * score = [
- * 	[成绩,学分]
- * 	[78, 3.0],
- * 	[78, 3.0],
- * 	[78, 3.0],
- * 	[78, 3.0]
- * ]
- */
-
-function score(grades) {
-
-	// 成绩
-	var score = [];
-	// 学分
-	var point = [];
-
-
-	for (var i = 0, len = grades.length; i < len; i++) {
-			var item = grades[i];
-			item.map((value, index) => {
-					if (index === 1) {
-							point.push(parseFloat(value));
-					} else {
-							score.push((parseFloat(value) - 50) / 10);
-					}
-			});
-	}
-
-	var sumScore = 0,
-			pointSum = 0;
-	for (var j = 0, len = score.length; j < len; j++) {
-			sumScore += score[j] * point[j];
-			pointSum += point[j];
-	}
-
-
-	return sumScore / pointSum;
-}
-
-// 得出成绩
-// console.log(
-// 	score([
-// 		[80, 1.5],
-// 		[99, 2],
-// 		[93, 1.5],
-// 		[75, 2],
-// 		[81, 3],
-// 		[81, 2],
-// 		[85, 2],
-// 		[85, 3],
-// 		[89, 2.5]
-// 	])
-// );
 
 function format(str) {
 	var reg = /\d+?(春|夏|秋|冬)季/;
@@ -81,6 +26,14 @@ function format(str) {
 		return record;
 }
 
+
+/**
+ * @author zifengb
+ * @creatime 2017-07-27T22:13:36+0800
+ * @param {string} str
+ * @returns {object}
+ */
+
 function getGrade (str){
 	var isValid = str.replace(/\s/, '');
 	if (!isValid) {
@@ -88,28 +41,27 @@ function getGrade (str){
 			return false;
 	}
 	var record = format(str);
-	// 成绩
-	var score = [];
-	// 学分
+	// 绩点
 	var point = [];
+	// 学分
+	var grade = [];
 
 	for (var i = 0, len = record.length; i < len; i++) {
 			var item = record[i];
-			// score.push((parseFloat(item['score100']) - 50) / 10);
-			score.push(parseFloat(item['point']));
-			point.push(parseFloat(item['grade']));
+			point.push(parseFloat(item['point']));
+			grade.push(parseFloat(item['grade']));
 	}
 
-	var sumScore = 0,
-			pointSum = 0;
-	for (var j = 0, len = score.length; j < len; j++) {
-			sumScore += score[j] * point[j];
-			pointSum += point[j];
+	var sumPG = 0, // 总学分绩
+			sumGrade = 0; // 总学分
+	for (var j = 0, len = point.length; j < len; j++) {
+			sumPG += point[j] * grade[j];
+			sumGrade += grade[j];
 	}
 
 
 	return {
 		record: record,
-		averagePoint: sumScore / pointSum
+		averagePoint: sumPG / sumGrade
 	};
 }
